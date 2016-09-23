a276ad7333c363df6bca7647d880e9e027fa86d2
Hannah Read <hannahdotread+gituser@gmail.com>
2016-09-23 15:23:37 -0400

    configure for racktest mixin and dotenv

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require 'rspec'
 require 'capybara/rspec'
+require 'dotenv'
 
 require_relative 'support/database_cleaner'
 require_relative '../app.rb'
@@ -9,9 +10,10 @@ require 'valid_attribute'
 require 'shoulda/matchers'
 
 set :environment, :test
-set :database, :test
+#set :database, :test
 
 ActiveRecord::Base.logger.level = 1
+Dotenv.load
 
 Capybara.app = Sinatra::Application
 
@@ -24,4 +26,10 @@ RSpec.configure do |config|
 
   config.order = :random
   Kernel.srand config.seed
+
+  config.include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
 end
