5ccd3c4fe56ad8deddcf8f8830eac3302dc0e3c6
Hannah Read <hannahdotread+gituser@gmail.com>
2016-09-23 20:10:43 -0400

    fixing post req

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader'
 require 'sinatra/flash'
 require 'json'
 require 'dotenv'
+require 'httparty'
 
 enable :sessions
 
@@ -24,19 +25,33 @@ Dotenv.load
 
 InvalidTokenError = Class.new(Exception)
 
+def adminize_message(msg)
+  message = {
+    "username": "chowdabot",
+    "channel": "#not-only-chowda",
+    "text": "<!channel> " + msg
+  }.to_json
+
+  HTTParty.post(
+    ENV['INCOMING_WEBHOOK_URL'],
+    :body => message,
+    :headers => {
+      'Content-type' => 'application/json'
+    }
+  )
+end
+
 get '/' do
   @title = "Hello World"
   erb :index
 end
 
 post '/' do
-  raise(InvalidTokenError) unless params[:token] == ENV['SLACK_TOKEN']
-
-  json_message = {
-    "response_type": "in_channel",
-    "text": "<!channel> " + params['text']
-  }.to_json
-
-  content_type :json
-  json_message
+  if params[:token] == ENV['SLACK_TOKEN']
+    adminize_message(params['text'])
+    status 200
+  else
+    raise InvalidTokenError
+    status 401
+  end
 end
